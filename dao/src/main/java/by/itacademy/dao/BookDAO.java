3a07b18e19c4c9a31741d421b3719e64f4a58233
masiuk-l <masiuk-l@ya.ru>
2017-11-07 22:10:30 +0300

    add pagination

--- dao/src/main/java/by/itacademy/dao/BookDAO.java
+++ dao/src/main/java/by/itacademy/dao/BookDAO.java
@@ -1,14 +1,14 @@
 package by.itacademy.dao;
 
 import by.itacademy.entities.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Project KR. Created by masiuk-l on 07.08.2017.
  */
-public interface BookDAO extends CrudRepository<Book, Integer> {
+public interface BookDAO extends PagingAndSortingRepository<Book, Integer> {
 
 
-//    List<Book> getCatalogPage(int pageNumber, int size);//todo переделать
+//   List<Book> ggetCatalogPage(int pageNumber, int size);//todo переделать
 
 }
