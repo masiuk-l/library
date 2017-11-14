da5e018e652209aba56a932535645f04998b4c55
masiuk-l <masiuk-l@ya.ru>
2017-11-15 00:38:25 +0300

    cleanup

--- dao/src/main/java/by/itacademy/dao/DAO.java
+++ dao/src/main/java/by/itacademy/dao/DAO.java
@@ -4,19 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
-import java.util.List;
 
 @NoRepositoryBean
 public interface DAO<T, ID extends Serializable> extends JpaRepository<T, ID> {
-    @Override
-    List<T> findAll();
 
-    @Override
-    <S extends T> S save(S s);
-
-    @Override
-    T findOne(ID id);
-
-    @Override
-    void deleteAll();
 }
